3393aeee0fae5037ec02144947110bb869f0b0f7
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-08-14 01:06:13 +0530

    add lazy loading

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
+import React,{ StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import MainLayout from './Layouts/MainLayout.jsx'
-import HomePage from './Routes/HomePage.jsx'
-import InfoPage from './Routes/InfoPage.jsx'
-import Base64Page from './Routes/Base64Page.jsx'
-import JsonPage from './Routes/JsonPage.jsx'
-import UserPage from './Routes/UserPage.jsx'
+
+const HomePage= React.lazy(()=> import('./Routes/HomePage.jsx'));
+const JsonPage= React.lazy(()=> import('./Routes/JsonPage.jsx'));
+const Base64Page= React.lazy(()=> import('./Routes/Base64Page.jsx'));
+const InfoPage= React.lazy(()=> import('./Routes/InfoPage.jsx'));
+const UserPage= React.lazy(()=> import('./Routes/UserPage.jsx'));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Suspense fallback={<div className='h-[100vh] w-[100vw] bg-[#020512] text-white flex items-center justify-center'><p className='text-xl'>Welcome to Dev ToolBox</p>
+    <p className='text-xs ml-2 mt-2 text-blue-400'>made by Subhodeep</p></div>}>
     <Routes>
       <Route element={<MainLayout/>}>
         <Route path='/' element={<HomePage/>}/>
@@ -22,6 +25,7 @@ createRoot(document.getElementById('root')).render(
         <Route path='/profile' element={<UserPage/>}/>
       </Route>
     </Routes>
+    </Suspense>
     </BrowserRouter>
   </StrictMode>,
 )
